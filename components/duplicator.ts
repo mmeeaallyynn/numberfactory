2535e696096ca7a29d12181455e8b2a6f694e986
Merlin Mengel <merlin.mengel@mail.de>
2025-11-30 12:29:15 +0100

    add one tick delay to the duplicator to prevent inconsistencies based on update order

--- components/duplicator.ts
+++ components/duplicator.ts
@@ -2,13 +2,19 @@ import { Component } from "./component.js";
 
 /** Duplicate the input number */
 export class Duplicator extends Component {
-    private outputs: number[] = [];
+    private outputs: (number | null)[] = [];
+    private waitingRoom: number | null = null;
 
     render(ctx: CanvasRenderingContext2D): void {
         this.renderDefault(ctx, "#0FA", "");
     }
 
-    tickState(): void {}
+    tickState(): void {
+        if (this.waitingRoom != null) {
+            this.outputs = [this.waitingRoom, this.waitingRoom];
+            this.waitingRoom = null;
+        }
+    }
     interact(components: Map<string, Component>): void {
         if (this.outputs.length > 0) {
             return;
@@ -17,7 +23,7 @@ export class Duplicator extends Component {
         for (let [_, component] of components) {
             let value = component.take();
             if (value != null) {
-                this.outputs = [value, value];
+                this.waitingRoom = value;
                 break;
             }
         }
