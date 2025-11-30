40385f47c0fdef1bbfab9f26fa13afa30f2484a5
Merlin Mengel <merlin.mengel@mail.de>
2025-11-30 16:12:47 +0100

    also show the content of bins

--- components/bin.ts
+++ components/bin.ts
@@ -1,15 +1,40 @@
 import { Component } from "./component.js";
+import { SQUARE_SIZE, TOTAL_SIZE } from "../draw.js";
 
 /** Ininite trash bin */
 export class Bin extends Component {
+    private previousRenderWidth = 0;
     render(ctx: CanvasRenderingContext2D): void {
-        this.renderDefault(ctx, "#AF0", "");
+        let text = `${this.content}`;
+        // Draw the text on top of the square
+        ctx.font = "30px arial";
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "center";
+        ctx.strokeStyle = "#FFF";
+        ctx.fillStyle = "#FFF";
+
+        ctx.clearRect(
+            TOTAL_SIZE * this.x +
+                SQUARE_SIZE / 2 -
+                this.previousRenderWidth / 2,
+            TOTAL_SIZE * this.y + SQUARE_SIZE / 2 - 30 / 2,
+            this.previousRenderWidth,
+            30,
+        );
+        this.previousRenderWidth = ctx.measureText(text).width;
+
+        this.renderDefault(ctx, "#AF0", text);
     }
 
     tickState(): void {}
     interact(components: Map<string, Component>): void {
+        let value: number | null = null;
         for (let [_, component] of components) {
-            let _ = component.take();
+            value = component.take();
+        }
+
+        if (value != null) {
+            this.content = value;
         }
     }
 }
